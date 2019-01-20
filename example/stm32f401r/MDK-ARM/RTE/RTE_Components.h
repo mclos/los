e9842330a51913b02f741d7ae06ac922cd53a658
los <pese56@126.com>
2019-01-20 13:50:02 +0800

    401 bubbleSort demo

---
+++ example/stm32f401r/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f401r' 
+ * Target:  'stm32f401r' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM4.h"
+
+
+#endif /* RTE_COMPONENTS_H */
